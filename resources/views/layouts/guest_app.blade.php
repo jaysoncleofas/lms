232488b10739abb7e9ea63bb52aa928233b33e1a
jaysoncleofas <jaysoncleofas22@gmail.com>
2018-09-21 05:45:19 +0800

    Change title heading

--- resources/views/layouts/guest_app.blade.php
+++ resources/views/layouts/guest_app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Learning Management system</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">

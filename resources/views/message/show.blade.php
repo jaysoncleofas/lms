1566c7f653907ee5df7a48d31f148636b5d8a9f4
jaysoncleofas <jaysoncleofas22@gmail.com>
2018-09-25 01:45:16 +0800

    Add profile pic to messages area

--- resources/views/message/show.blade.php
+++ resources/views/message/show.blade.php
@@ -18,17 +18,17 @@
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="">
-                                    @foreach ($messages as $message) 
+                                    @foreach ($messages as $message)
                                     <div class="text-center"><small>{{$message->created_at->toDayDateTimeString()}}</small></div>
                                         @if ($message->user_id != auth()->user()->id)
                                         <div class="d-flex justify-content-start media">
                                             <!-- Avatar -->
-                                            <img class="img-fluid rounded-circle z-depth-1 mr-2" style="height:50px;width:50px;" src="{{$message->user->photo ? '' : asset('images/profile_pic.png')}}">
-            
+                                            <img class="img-fluid rounded-circle z-depth-1 mr-2" style="height:50px;width:50px;" src="{{$message->user->avatar ? asset('storage/avatars/'.$message->user->avatar) : asset('images/profile_pic.png')}}">
+
                                                 <p class="grey lighten-3 rounded p-3 w-75"><strong>{{$message->user->name()}}:</strong> {{$message->message}}</p>
                                         </div>
 
-                                        @else 
+                                        @else
                                             {{-- <div class="text-left"><small>{{$message->created_at->toDayDateTimeString()}}</small></div> --}}
                                             <div class="d-flex justify-content-end">
                                                 <p class="primary-color white-text rounded p-3 grey-white w-75 ">
@@ -39,13 +39,13 @@
                                     @endforeach
                                 </div>
                             </div>
-                        </div>  
+                        </div>
                         <!-- New message -->
                         <form action="{{route('message.reply', $conversation->id)}}" method="POST">
                             @csrf
                         <div class="row">
                             <div class="col-lg-12">
-                                
+
                                 <div class="d-flex flex-row">
                                     <div class="md-form chat-message-type">
                                         <textarea type="text" id="message" name="message" class="md-textarea form-control" rows="3"></textarea>
@@ -56,11 +56,11 @@
                                         <button class="btn btn-primary">Send</button>
                                     </div>
                                 </div>
-                                    
+
                             </div>
                         </div>
                          </form>
-                        <!-- /.New message -->                          
+                        <!-- /.New message -->
                     </div>
                 </div>
             </div>
